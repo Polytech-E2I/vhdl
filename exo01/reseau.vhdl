a7ddcc705ea86dced50443b26b8745fe23246102
Robin SHAMSNEJAD <rsham@free.fr>
2022-09-26 17:22:43 +0200

    Add reseau

--- exo01/reseau.vhdl
+++ exo01/reseau.vhdl
@@ -12,3 +12,13 @@ entity reseau is
         Dout:   out integer
     );
 end reseau;
+
+architecture impl02 of reseau is
+    signal S1, S2, S3, S4, S5, S6: integer;
+    begin
+        AIG1: entity work.aiguilleur port map (Ain => Ain, Bin => Bin, C => Ctrl(4), Aout => S1, Bout => S2);
+        AIG2: entity work.aiguilleur port map (Ain => Cin, Bin => Din, C => Ctrl(3), Aout => S3, Bout => S4);
+        AIG3: entity work.aiguilleur port map (Ain => S2, Bin => S3, C => Ctrl(2), Aout => S5, Bout => S6);
+        AIG4: entity work.aiguilleur port map (Ain => S1, Bin => S5, C => Ctrl(1), Aout => Aout, Bout => Bout);
+        AIG5: entity work.aiguilleur port map (Ain => S6, Bin => S4, C => Ctrl(0), Aout => Cout, Bout => Dout);
+end architecture impl02;
---
+++ exo01/reseau_tb.vhdl
@@ -0,0 +1,18 @@
+entity reseau_tb is
+end reseau_tb;
+
+architecture tb of reseau_tb is
+    signal Ain, Bin, Cin, Din: integer;
+    signal Ctrl: bit_vector(4 downto 0);
+    signal Aout, Bout, Cout, Dout: integer;
+begin
+    RES: entity work.reseau port map (Ain => Ain, Bin => Bin, Cin => Cin, Din => Din, Ctrl => Ctrl, Aout => Aout, Bout => Bout, Cout => Cout, Dout => Dout);
+
+    Ain <= 1;
+    Bin <= 2;
+    Cin <= 3;
+    Din <= 4;
+
+    -- Ctrl := ('0, '0, '0, '0, '0');
+    Ctrl <= "00000";
+end tb;
