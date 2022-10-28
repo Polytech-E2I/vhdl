98fa4eca889d767dfd9c4e9365323ab9db756262
Robin SHAMSNEJAD <rsham@free.fr>
2022-10-28 20:06:10 +0200

    Add sousnbit

---
+++ TP1/exo03-05/sousnbit.vhdl
@@ -0,0 +1,42 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sousnbit is
+    generic(
+        N:          natural
+    );
+    port(
+        Ain, Bin:   in std_logic_vector(N-1 downto 0);
+        S:          out std_logic_vector(N-1 downto 0);
+        C:          out std_logic
+    );
+end;
+
+architecture sousnimpl of sousnbit is
+    signal Carry_int: std_logic_vector(N downto 0) := (others => '0');
+    component sous1bit is
+        port
+        (
+            Ain, Bin, Cin:  in std_logic;
+            Cout, S:        out std_logic
+        );
+    end component;
+begin
+    Carry_int(0) <= '0';
+    SOUSN_inst: for I in 0 to N-1 generate
+        ADD: sous1bit port map(
+            -- Ain => Ain(I),
+            -- Bin => Bin(I),
+            -- Cin => Carry_int(I),
+            -- Cout => Carry_int(I+1),
+            -- S => S(I)
+            Ain(I),
+            Bin(I),
+            Carry_int(I),
+            Carry_int(I+1),
+            S(I)
+        );
+    end generate;
+
+    C <= Carry_int(N);
+end;
---
+++ TP1/exo03-05/sousnbit_tb.vhdl
@@ -0,0 +1,50 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sousnbit_tb is
+end;
+
+architecture tb of sousnbit_tb is
+    --- UUT component
+    component sousnbit
+        generic(
+            N:          natural
+        );
+        port(
+            Ain, Bin:   in std_logic_vector(N-1 downto 0);
+            S:          out std_logic_vector(N-1 downto 0);
+            C:          out std_logic
+        );
+    end component;
+
+
+    constant N:     natural := 4;
+    --- UUT input signals
+    signal Ain:     std_logic_vector(N-1 downto 0) := (others => 'U');
+    signal Bin:     std_logic_vector(N-1 downto 0) := (others => 'U');
+    --- UUT output signals
+    signal S:       std_logic_vector(N-1 downto 0);
+    signal C:       std_logic;
+
+begin
+    ADDN: sousnbit
+    generic map(
+        N => N
+    )
+    port map(
+        Ain     => Ain,
+        Bin     => Bin,
+        S       => S,
+        C       => C
+    );
+
+    Ain <=
+        std_logic_vector(to_unsigned(9, Ain'length)),
+        std_logic_vector(to_unsigned(3, Ain'length)) after 1 fs,
+        std_logic_vector(to_unsigned(0, Ain'length)) after 2 fs;
+    Bin <=
+        std_logic_vector(to_unsigned(9, Bin'length)),
+        std_logic_vector(to_unsigned(7, Bin'length)) after 1 fs,
+        std_logic_vector(to_unsigned(0, Bin'length)) after 2 fs;
+end tb;
