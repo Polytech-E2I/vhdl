3f362ec09cbca111bd5d54a29130fdc5f3787152
Robin SHAMSNEJAD <rsham@free.fr>
2022-12-01 20:57:57 +0100

    Add tb

--- TP4/exo01/pgcounter_tb.vhdl
+++ TP4/exo01/pgcounter_tb.vhdl
@@ -6,6 +6,7 @@ entity pgcounter_tb is
 end;
 
 architecture tb of pgcounter_tb is
+    constant filename:  string := "testfile.txt";
     constant addrsize:  natural := 4;
     constant datasize:  natural := 8;
 
@@ -31,9 +32,42 @@ architecture tb of pgcounter_tb is
     -- UUT output signals
     signal inst:    std_logic_vector(datasize-1 downto 0);
 
+    -- NEEDED TO FILL UP MEMORY ------------------------------------------------
+    component MA_RAM is
+        generic(
+            load_file_name : string;
+            dta : integer := 8;
+            adr : integer := 4
+        );
+        port(
+            address : in std_logic_vector(adr-1 downto 0);
+            datain :  in std_logic_vector(dta-1 downto 0);
+            wr_ena :  in std_logic;
+            dataout : out std_logic_vector(dta-1 downto 0)
+        );
+    end component;
+
+    signal address : std_logic_vector(addrsize-1 downto 0);
+    signal datain :  std_logic_vector(datasize-1 downto 0);
+    signal wr_ena :  std_logic := '0';
+    signal dataout : std_logic_vector(datasize-1 downto 0);
+    ----------------------------------------------------------------------------
+
     constant clock_period: time := 1 fs;
 
 begin
+    RAM: MA_RAM
+        generic map(
+            load_file_name => filename,
+            adr => addrsize,
+            dta => datasize
+        )
+        port map(
+            address => address,
+            datain => datain,
+            wr_ena => wr_ena,
+            dataout => dataout
+        );
     UUT: pgcounter
         generic map(
             addrsize => addrsize,
@@ -50,10 +84,22 @@ begin
 
     CASES: process
     begin
+        -- Fill up memory
+        wr_ena <= '1';
+        for i in 0 to 15 loop
+            address <= std_logic_vector(to_unsigned(i, address'length));
+            datain <= std_logic_vector(to_unsigned(i+30, datain'length));
+            wait for 1 fs;
+        end loop;
+        datain <= (others => 'U');
+        wr_ena <= '0';
+
         wait for 2 fs;
         addr <= std_logic_vector(to_unsigned(13, addr'length));
         wait for 1 fs;
         jmp <= '1';
-        wait for 1 fs;
+        wait for 10 fs;
+        jmp <= '0';
+        wait for 100 fs;
     end process;
 end;
